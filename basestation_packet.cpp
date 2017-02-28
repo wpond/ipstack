e36af751f570eb7e708dfeeee3ffea201ac50cd0
Will <contact@williampond.co.uk>
2017-02-28 23:06:39 +0000

    Add packet parent option

--- basestation_packet.cpp
+++ basestation_packet.cpp
@@ -1,6 +1,7 @@
 #include "basestation_packet.h"
 
 #include <cstring>
+#include <stdexcept>
 
 namespace basestation
 {
@@ -10,6 +11,23 @@ Packet::Packet(const char* data, int size)
 {
     mData = new char[size];
     memcpy(mData, data, size);
+}
+
+Packet::Packet(std::shared_ptr<Packet> packet, int start, int size)
+    : mParent(packet)
+{
+    if (!packet)
+    {
+        throw std::runtime_error("Cannot create packet, passed NULL parent packet");
+    }
+
+    if (packet->size() > (size + start))
+    {
+        throw std::runtime_error("Cannot create packet, passed too small parent packet");
+    }
+
+    mData = packet->mData + start;
+    mSize = (size) ? size : packet->mSize - start;
 }
 
 Packet::~Packet()
--- basestation_packet.h
+++ basestation_packet.h
@@ -1,6 +1,8 @@
 #ifndef BASESTATION_PACKET_H
 #define BASESTATION_PACKET_H
 
+#include <memory>
+
 namespace basestation
 {
 
@@ -8,6 +10,7 @@ class Packet
 {
 public:
     Packet(const char* data, int size);
+    Packet(std::shared_ptr<Packet> packet, int start=0, int size=0);
     ~Packet();
 
     const char* data() const;
@@ -15,6 +18,7 @@ public:
 private:
     char* mData;
     int mSize;
+    std::shared_ptr<Packet> mParent;
 };
 
 }
