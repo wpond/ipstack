6282e74407ffe9966cbb81f73247c30585865410
Will <contact@williampond.co.uk>
2017-06-10 15:50:27 +0100

    mac address

---
+++ networkutils/networkutils_macaddress.cpp
@@ -0,0 +1,60 @@
+#include "networkutils_macaddress.h"
+
+#include <cstring>
+
+namespace networkutils
+{
+
+MacAddress::MacAddress()
+{
+    memset(mAddress, 0, sizeof(mAddress));
+}
+
+MacAddress::MacAddress(uint8_t* value)
+{
+    memcpy(mAddress, value, sizeof(mAddress));
+}
+
+MacAddress::MacAddress(
+    uint8_t byte1,
+    uint8_t byte2,
+    uint8_t byte3,
+    uint8_t byte4,
+    uint8_t byte5,
+    uint8_t byte6)
+{
+    mAddress[0] = byte1;
+    mAddress[1] = byte2;
+    mAddress[2] = byte3;
+    mAddress[3] = byte4;
+    mAddress[4] = byte5;
+    mAddress[5] = byte6;
+}
+
+MacAddress::MacAddress(const MacAddress& that)
+{
+    memcpy(mAddress, that.mAddress, sizeof(mAddress));
+}
+
+MacAddress& MacAddress::operator=(const MacAddress& rhs)
+{
+    memcpy(mAddress, rhs.mAddress, sizeof(mAddress));
+    return *this;
+}
+
+uint8_t* MacAddress::value()
+{
+    return mAddress;
+}
+
+const uint8_t* MacAddress::value() const
+{
+    return mAddress;
+}
+
+bool MacAddress::operator==(const MacAddress& rhs)
+{
+    return memcmp(mAddress, rhs.mAddress, sizeof(mAddress));
+}
+
+}
---
+++ networkutils/networkutils_macaddress.h
@@ -0,0 +1,35 @@
+#ifndef NETWORKUTILS_MACADDRESS_H
+#define NETWORKUTILS_MACADDRESS_H
+
+#include <cstdint>
+
+namespace networkutils
+{
+
+class MacAddress
+{
+public:
+    MacAddress();
+    MacAddress(uint8_t* value);
+    MacAddress(
+        uint8_t byte1,
+        uint8_t byte2,
+        uint8_t byte3,
+        uint8_t byte4,
+        uint8_t byte5,
+        uint8_t byte6);
+
+    MacAddress(const MacAddress& that);
+    MacAddress& operator=(const MacAddress& rhs);
+
+    uint8_t* value();
+    const uint8_t* value() const;
+
+    bool operator==(const MacAddress& rhs);
+private:
+    uint8_t mAddress[6];
+};
+
+}
+
+#endif
